1d87592e82c904fd08dd2dd8ac9ca73c8d1f9cdf
Dathan <felipedathan@gmail.com>
2023-02-26 13:24:27 +0800

    - add assets. - add data files. - create Models struct. - parse json file in ContentModel. - parse style.html in ContentModel.

--- Learning App/ViewModels/ContentModel.swift
+++ Learning App/ViewModels/ContentModel.swift
@@ -8,5 +8,59 @@
 import Foundation
 
 class ContentModel: ObservableObject {
+    @Published var modules = [Module]()
+    var styleData:Data?
     
+    init() {
+        self.getLocalData()
+    }
+    
+    func getLocalData() {
+        
+        // get url to the json file
+        let jsonURL = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        // unwrap jsonURL
+        guard let jsonURL = jsonURL else {
+            return print("No json file found")
+        }
+        
+        do {
+            
+            // turn the file into a data object
+            let jsonData = try Data(contentsOf: jsonURL)
+            
+            // decode the json file into an array of modules
+            let jsonDecoder = JSONDecoder()
+            let modules = try jsonDecoder.decode([Module].self, from: jsonData)
+            
+            // assign parsed modules to modules property
+            self.modules = modules
+        }
+        
+        // log the error
+        catch {
+            print("Couldn't parse json data")
+        }
+        
+        // parse style data
+        let styleURL = Bundle.main.url(forResource: "style", withExtension: "html")
+        
+        // unwrap styleURL
+        guard let styleURL = styleURL else {
+            return print("No html file found")
+        }
+        
+        do {
+            // read the file into a data object
+            let styleData = try Data(contentsOf: styleURL)
+            
+            // assign parsed style data to styleData property
+            self.styleData = styleData
+        }
+        
+        catch {
+            print("Couldn't parse html data")
+        }
+    }
 }
